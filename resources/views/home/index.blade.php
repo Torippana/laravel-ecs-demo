be0513ac9795666a52f50ed9637fc42773464385
makotokato2 <makoto.kato2@persol.co.jp>
2020-12-11 11:43:16 +0900

    [fix]phpのコンテナのビルドが通らなかったので修正

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
     <h3>Laravel on ECS Demo</h3>
+    <ul>
+        <li>laravelソースコードをコンテナ内にコピー</li>
+        <li>コンテナ内でセットアップしたimageを作成</li>
+        <li>ECRにimageをPUSH</li>
+        <li>ECS fargateでpull</li>
+    </ul>
 @endsection
